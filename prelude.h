b8e3a1d6bb56ce4ae7d2ffe817fe5b6a7c5cf217
jay <jayfrasergames+git@gmail.com>
2020-05-14 21:35:18 +0100

    initial commit

---
+++ prelude.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t  i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef float  f32;
+typedef double f64;
+
+struct v2
+{
+	union { f32 x, w; };
+	union { f32 y, h; };
+};
+
+struct v3
+{
+	f32 x, y, z;
+};
+
+struct v4
+{
+	union { f32 x, r; };
+	union { f32 y, g; };
+	union { f32 z, b; };
+	union { f32 w, a; };
+};
+
+struct v2_u32
+{
+	union { u32 x; u32 w; };
+	union { u32 y; u32 h; };
+};
+
+struct m3x2
+{
+	union
+	{
+		v2 cols[3];
+		f32 elems[6];
+	};
+};
+
+#define ARRAY_SIZE(xs) (sizeof(xs)/sizeof(xs[0]))
+#define OFFSET_OF(struct_type, member) ((size_t)(&((struct_type*)0)->member))
+
+#ifdef LIBRARY
+	#define LIBRARY_EXPORT extern "C" __declspec(dllexport)
+#else
+	#define LIBRARY_EXPORT
+#endif
