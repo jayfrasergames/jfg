069a6103fc46b7b5a9a776a1aa7f05768e41b8ed
jay <jayfrasergames+git@gmail.com>
2020-06-10 23:23:56 +0100

    added vector math/2x2 matrix

--- jfg_math.h
+++ jfg_math.h
@@ -1,6 +1,7 @@
 #ifndef JFG_MATH_H
 #define JFG_MATH_H
 
+#include "prelude.h"
 #include <math.h>
 
 // a tad excessive...
@@ -26,6 +27,31 @@ static inline f32 lerp(f32 start, f32 end, f32 weight) {
 
 static inline f32 clamp(f32 low, f32 high, f32 val) {
 	return min(max(low, val), high);
+}
+
+struct m2
+{
+	union {
+		struct {
+			f32 _00, _10, _01, _11;
+		};
+		f32 elements[4];
+		v2 rows[2];
+	};
+
+	static m2 rotation(f32 theta)
+	{
+		f32 c = cosf(theta), s = sinf(theta);
+		m2 m;
+		m._00 = c; m._10 = -s;
+		m._01 = s; m._11 =  c;
+		return m;
+	}
+};
+
+v2 operator*(m2 m, v2 v)
+{
+	return V2_f32(m._00 * v.x + m._10 * v.y, m._01 * v.x + m._11 * v.y);
 }
 
 #endif
--- prelude.h
+++ prelude.h
@@ -17,50 +17,6 @@ typedef int64_t i64;
 typedef float  f32;
 typedef double f64;
 
-#define V2(Type) \
-	struct v2_##Type \
-	{ \
-		union { Type x, w; }; \
-		union { Type y, h; }; \
-	};
-
-V2(f32); V2(u8); V2(u16); V2(u32); V2(u64);
-V2(f64); V2(i8); V2(i16); V2(i32); V2(i64);
-
-#undef V2
-
-typedef v2_f32 v2;
-
-struct v3
-{
-	f32 x, y, z;
-};
-
-#define V4(Type) \
-	struct v4_##Type \
-	{ \
-		union { Type x, r; }; \
-		union { Type y, g; }; \
-		union { Type z, b; }; \
-		union { Type w, a; }; \
-	};
-
-V4(f32); V4(u8); V4(u16); V4(u32); V4(u64);
-V4(f64); V4(i8); V4(i16); V4(i32); V4(i64);
-
-#undef V4
-
-typedef v4_f32 v4;
-
-struct m3x2
-{
-	union
-	{
-		v2 cols[3];
-		f32 elems[6];
-	};
-};
-
 #define ASSERT(expr) assert(expr)
 #define ARRAY_SIZE(xs) (sizeof(xs)/sizeof(xs[0]))
 #define OFFSET_OF(struct_type, member) ((size_t)(&((struct_type*)0)->member))
@@ -71,5 +27,173 @@ struct m3x2
 #else
 	#define LIBRARY_EXPORT
 #endif
+
+#define VEC_TYPES \
+	VEC_TYPE(u8) VEC_TYPE(u16) VEC_TYPE(u32) VEC_TYPE(u64) \
+	VEC_TYPE(i8) VEC_TYPE(i16) VEC_TYPE(i32) VEC_TYPE(i64) \
+	VEC_TYPE(f32) VEC_TYPE(f64)
+
+// forward declare vec types
+#define VEC_TYPE(type) struct v2_##type; struct v3_##type; struct v4_##type;
+VEC_TYPES
+#undef VEC_TYPE
+
+// declare vec types
+#define VEC_TYPE(type) \
+	struct v2_##type \
+	{ \
+		union { type x, w; }; \
+		union { type y, h; }; \
+		operator bool() { return x || y; } \
+		explicit operator v2_u8(); \
+		explicit operator v2_u16(); \
+		explicit operator v2_u32(); \
+		explicit operator v2_u64(); \
+		explicit operator v2_i8(); \
+		explicit operator v2_i16(); \
+		explicit operator v2_i32(); \
+		explicit operator v2_i64(); \
+		explicit operator v2_f32(); \
+		explicit operator v2_f64(); \
+	}; \
+	struct v3_##type \
+	{ \
+		union { type x, w, r; }; \
+		union { type y, h, g; }; \
+		union { type z, d, b; }; \
+		operator bool() { return x || y || z; } \
+		explicit operator v3_u8(); \
+		explicit operator v3_u16(); \
+		explicit operator v3_u32(); \
+		explicit operator v3_u64(); \
+		explicit operator v3_i8(); \
+		explicit operator v3_i16(); \
+		explicit operator v3_i32(); \
+		explicit operator v3_i64(); \
+		explicit operator v3_f32(); \
+		explicit operator v3_f64(); \
+	}; \
+	struct v4_##type \
+	{ \
+		union { type x, r; }; \
+		union { type y, g; }; \
+		union { type z, b; }; \
+		union { type w, a; }; \
+		operator bool() { return x || y || z || w; } \
+		explicit operator v4_u8(); \
+		explicit operator v4_u16(); \
+		explicit operator v4_u32(); \
+		explicit operator v4_u64(); \
+		explicit operator v4_i8(); \
+		explicit operator v4_i16(); \
+		explicit operator v4_i32(); \
+		explicit operator v4_i64(); \
+		explicit operator v4_f32(); \
+		explicit operator v4_f64(); \
+	};
+VEC_TYPES
+#undef VEC_TYPE
+
+// define "constructors"
+#define VEC_TYPE(type) \
+	v2_##type V2_##type(type x, type y) { v2_##type v = { x, y }; return v; } \
+	v3_##type V3_##type(type x, type y, type z) { v3_##type v = { x, y, z }; return v; } \
+	v4_##type V4_##type(type x, type y, type z, type w) { v4_##type v = { x, y, z, w }; return v; }
+VEC_TYPES
+#undef VEC_TYPE
+
+// define cast operators
+#define VEC_TYPE(type) \
+	v2_##type::operator v2_u8()  { return V2_u8 ((u8)x,  (u8)y);  }; \
+	v2_##type::operator v2_u16() { return V2_u16((u16)x, (u16)y); }; \
+	v2_##type::operator v2_u32() { return V2_u32((u32)x, (u32)y); }; \
+	v2_##type::operator v2_u64() { return V2_u64((u64)x, (u64)y); }; \
+	v2_##type::operator v2_i8()  { return V2_i8 ((i8)x,  (i8)y);  }; \
+	v2_##type::operator v2_i16() { return V2_i16((i16)x, (i16)y); }; \
+	v2_##type::operator v2_i32() { return V2_i32((i32)x, (i32)y); }; \
+	v2_##type::operator v2_i64() { return V2_i64((i64)x, (i64)y); }; \
+	v2_##type::operator v2_f32() { return V2_f32((f32)x, (f32)y); }; \
+	v2_##type::operator v2_f64() { return V2_f64((f64)x, (f64)y); }; \
+	v3_##type::operator v3_u8()  { return V3_u8 ((u8)x,  (u8)y,  (u8)z ); }; \
+	v3_##type::operator v3_u16() { return V3_u16((u16)x, (u16)y, (u16)z); }; \
+	v3_##type::operator v3_u32() { return V3_u32((u32)x, (u32)y, (u32)z); }; \
+	v3_##type::operator v3_u64() { return V3_u64((u64)x, (u64)y, (u64)z); }; \
+	v3_##type::operator v3_i8()  { return V3_i8 ((i8)x,  (i8)y,  (i8)z);  }; \
+	v3_##type::operator v3_i16() { return V3_i16((i16)x, (i16)y, (i16)z); }; \
+	v3_##type::operator v3_i32() { return V3_i32((i32)x, (i32)y, (i32)z); }; \
+	v3_##type::operator v3_i64() { return V3_i64((i64)x, (i64)y, (i64)z); }; \
+	v3_##type::operator v3_f32() { return V3_f32((f32)x, (f32)y, (f32)z); }; \
+	v3_##type::operator v3_f64() { return V3_f64((f64)x, (f64)y, (f64)z); }; \
+	v4_##type::operator v4_u8()  { return V4_u8 ((u8)x,  (u8)y,  (u8)z,  (u8)w ); }; \
+	v4_##type::operator v4_u16() { return V4_u16((u16)x, (u16)y, (u16)z, (u16)w); }; \
+	v4_##type::operator v4_u32() { return V4_u32((u32)x, (u32)y, (u32)z, (u32)w); }; \
+	v4_##type::operator v4_u64() { return V4_u64((u64)x, (u64)y, (u64)z, (u64)w); }; \
+	v4_##type::operator v4_i8()  { return V4_i8 ((i8)x,  (i8)y,  (i8)z,  (i8)w);  }; \
+	v4_##type::operator v4_i16() { return V4_i16((i16)x, (i16)y, (i16)z, (i16)w); }; \
+	v4_##type::operator v4_i32() { return V4_i32((i32)x, (i32)y, (i32)z, (i32)w); }; \
+	v4_##type::operator v4_i64() { return V4_i64((i64)x, (i64)y, (i64)z, (i64)w); }; \
+	v4_##type::operator v4_f32() { return V4_f32((f32)x, (f32)y, (f32)z, (f32)w); }; \
+	v4_##type::operator v4_f64() { return V4_f64((f64)x, (f64)y, (f64)z, (f64)w); };
+VEC_TYPES
+#undef VEC_TYPE
+
+// define operator overloads
+#define VEC_TYPE(type) \
+	v2_##type operator+(v2_##type a, v2_##type b) \
+	{ \
+		return V2_##type(a.x + b.x, a.y + b.y); \
+	} \
+	v2_##type operator-(v2_##type a, v2_##type b) \
+	{ \
+		return V2_##type(a.x - b.x, a.y - b.y); \
+	} \
+	v2_##type operator*(v2_##type a, v2_##type b) \
+	{ \
+		return V2_##type(a.x * b.x, a.y * b.y); \
+	} \
+	v2_##type operator/(v2_##type a, v2_##type b) \
+	{ \
+		return V2_##type(a.x / b.x, a.y / b.y); \
+	} \
+	v2_##type operator+(type s, v2_##type v) \
+	{ \
+		return V2_##type(s + v.x, s + v.y); \
+	} \
+	v2_##type operator-(type s, v2_##type v) \
+	{ \
+		return V2_##type(s - v.x, s - v.y); \
+	} \
+	v2_##type operator*(type s, v2_##type v) \
+	{ \
+		return V2_##type(s * v.x, s * v.y); \
+	} \
+	v2_##type operator/(type s, v2_##type v) \
+	{ \
+		return V2_##type(s / v.x, s / v.y); \
+	} \
+	v2_##type operator+(v2_##type v, type s) \
+	{ \
+		return V2_##type(v.x + s, v.y + s); \
+	} \
+	v2_##type operator-(v2_##type v, type s) \
+	{ \
+		return V2_##type(v.x - s, v.y - s); \
+	} \
+	v2_##type operator*(v2_##type v, type s) \
+	{ \
+		return V2_##type(v.x * s, v.y * s); \
+	} \
+	v2_##type operator/(v2_##type v, type s) \
+	{ \
+		return V2_##type(v.x / s, v.y / s); \
+	}
+VEC_TYPES
+#undef VEC_TYPE
+
+#undef VEC_TYPES
+
+typedef v2_f32 v2;
+typedef v3_f32 v3;
+typedef v4_f32 v4;
 
 #endif
